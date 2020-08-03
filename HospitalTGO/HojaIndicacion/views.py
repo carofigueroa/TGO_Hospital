d88ca0e457a15353f2b1eb79bfcb06d9592d2de4
carofigueroa <54367757+carofigueroa@users.noreply.github.com>
2020-08-02 22:34:20 -0400

    Update views.py

--- HospitalTGO/HojaIndicacion/views.py
+++ HospitalTGO/HojaIndicacion/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from HojaIndicacion.models import paciente, hoja_indicaciones
 
 # Create your views here.
+
+def kpis(request):
+    embarazo=paciente.objects.all()
+
+    return render(request, 'KPI.html',{
+        'embarazadas': embarazo
+    })
