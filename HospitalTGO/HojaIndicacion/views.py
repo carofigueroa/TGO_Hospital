4b0c54d96615198b7762211df0ce1d9487cf7842
carofigueroa <54367757+carofigueroa@users.noreply.github.com>
2020-08-03 00:58:16 -0400

    Update views.py

--- HospitalTGO/HojaIndicacion/views.py
+++ HospitalTGO/HojaIndicacion/views.py
@@ -4,8 +4,12 @@ from HojaIndicacion.models import paciente, hoja_indicaciones
 # Create your views here.
 
 def kpis(request):
-    embarazo=paciente.objects.all()
+    embarazo=paciente.objects.filter(embarazada=True)
+    no_embarazo=paciente.objects.filter(embarazada=False)
+    cirugias=hoja_indicaciones.objects.all()
 
     return render(request, 'KPI.html',{
-        'embarazadas': embarazo
+        'embarazo': embarazo,
+        'no_embarazo':no_embarazo,
+        'cirugias': cirugias
     })
